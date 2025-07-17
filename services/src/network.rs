a0eb8aff8a18fb197ee131a3058bc2cd1444ecbc
Oscar Pepper <oscarpepper@zingolabs.org>
2025-07-17 17:14:36 +0100

    make pick_unused_port pub

--- services/src/network.rs
+++ services/src/network.rs
@@ -77,7 +77,7 @@ impl ActivationHeights {
 
 /// Checks `fixed_port` is not in use.
 /// If `fixed_port` is `None`, returns a random free port between 15_000 and 25_000.
-pub(crate) fn pick_unused_port(fixed_port: Option<Port>) -> Port {
+pub fn pick_unused_port(fixed_port: Option<Port>) -> Port {
     if let Some(port) = fixed_port {
         if !portpicker::is_free(port) {
             panic!("Fixed port is not free!");
